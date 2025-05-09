967da07cc8d8478600808633e7e002739147a45a
MichniaUni <162065861+MichniaUni@users.noreply.github.com>
2025-05-09 17:38:45 +0100

    Disable the Valide function

--- taskilo/src/components/ModalNewTask/index.tsx
+++ taskilo/src/components/ModalNewTask/index.tsx
@@ -506,12 +506,20 @@ const ModalNewTask = ({ isOpen, onClose, id = null, task }: Props) => {
           onChange={(e) => setProjectId(e.target.value)}
           />
         )}
-        <button
+        {/* <button
           type="submit"
           className={`focus-offset-2 mt-4 flex w-full justify-center rounded-md border border-transparent bg-blue-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 ${
             !isFormValid() || isLoading ? "cursor-not-allowed opacity-50" : ""
           }`}
           disabled={!isFormValid() || isLoading}
+        >
+          {isLoading ? "Saving..." : task ? "Update Task" : "Create Task"}
+        </button> */}
+        <button
+          type="submit"
+          className={`focus-offset-2 mt-4 flex w-full justify-center rounded-md border border-transparent bg-blue-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 ${
+            isLoading ? "cursor-not-allowed opacity-50" : ""
+          }`}
         >
           {isLoading ? "Saving..." : task ? "Update Task" : "Create Task"}
         </button>
