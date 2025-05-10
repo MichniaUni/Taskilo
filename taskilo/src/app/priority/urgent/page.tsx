878a1b118b9ec35518b168bd8fce46fc53b82e3d
MichniaUni <162065861+MichniaUni@users.noreply.github.com>
2025-05-10 21:25:06 +0100

    Reverse the Urgent changes

--- taskilo/src/app/priority/urgent/page.tsx
+++ taskilo/src/app/priority/urgent/page.tsx
@@ -1,13 +1,3 @@
-// import React from "react";
-// import ReusablePriorityPage from "../reusablePriorityPage";
-// import { Priority } from "@/state/api";
-
-// const Urgent = () => {
-//   return <ReusablePriorityPage priority={Priority.Urgent} />;
-// };
-
-// export default Urgent;
-
 import React from "react";
 import ReusablePriorityPage from "../reusablePriorityPage";
 import { Priority } from "@/state/api";
